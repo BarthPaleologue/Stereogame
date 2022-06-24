62bea52d97a0d1031b0242572e0d5e9ee00113d5
Kenza <kenza.difallah@gmail.com>
2022-06-24 15:20:50 +0000

    test ball

--- game/ball.py
+++ game/ball.py
@@ -1,9 +1,9 @@
-from feather.shapes import Shape
+from feather.shapes import Sphere, Rectangle
 
-class Ball(Shape):
+class Ball(Sphere):
         def __init__(self, name, position, velocity, acceleration,  scene, flip = None):
 
-            Shape.__init__(self, name, scene)
+            Sphere.__init__(self, name, scene)
             self.position = position
             self.velocity = velocity
             self.acceleration = acceleration
@@ -18,6 +18,21 @@ class Ball(Shape):
             self.translate(self.position[0]+self.velocity[0], self.position[1]+self.velocity[1], self.position[2]+self.velocity[2])
             newVelocity = [self.velocity[0]+self.acceleration[0], self.velocity[1]+self.acceleration[1], self.velocity[2]+self.acceleration[2]]
             self.velocity = newVelocity
+
+        def getVelocity(self):
+            return self.velocity
+
+        def getAcceleration(self):
+            return self.acceleration
+
+        def showTrajectory(self):
+            #creer rectangle fin + prend position initiale position balle 
+            #rotate le rectangle selon velocity + tracer uniquement une partie de la trajectoire
+            traj = Rectangle('traj', False, self.scene)
+            traj.setPosition(self.position[0], self.position[1], self.position[2])
+            traj.setScaling(0.5, 0.005, 1)
+            traj.setRotationY(45)
+
 
 
             
