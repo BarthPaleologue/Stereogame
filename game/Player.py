00f4524e34ce292d85d67d1f2e66496e0d7cee4d
Ryan Borhani <ryan.borhani@telecom-paris.fr>
2022-06-24 11:11:38 +0200

    Player - invicible

--- game/Player.py
+++ game/Player.py
@@ -1,12 +1,14 @@
 class Player :
     
-    def __init__(self, position, angleVision, batte, oeilGauche, oeilDroit, gamepad) :
+    def __init__(self, position, angleVision, batte, oeilGauche, oeilDroit, gamepad, invincible, state) :
         self.position = position
         self.angleVision = angleVision
         self.batte = batte
         self.oeilGauche = oeilGauche
         self.oeilDroit = oeilDroit
         self.gamepad = gamepad
+        self.invincible = invincible
+        #self.state = state
 
     def getPosition(self) :
         return self.position
@@ -25,6 +27,12 @@ class Player :
     
     def getGamepad(self) :
         return self.gamepad
+    
+    def setinvincible(self) :
+        if self.invincible :
+            self.invincible = False
+        else :
+            self.invincible = True
     
     def reverseView(self) :
         self.oeilDroit, self.oeilGauche = self.oeilGauche, self.oeilDroit
